a3355d840ac697a640625b9f7ebf9743ed60d4d5
Keks <keks@htmlacademy.ru>
2023-11-20 13:26:53 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,7 @@
+//import { generatePhotosArray } from './data.js';
+import { createPictureElement } from './pictureThumbnailsRenderer.js';
+
+const photos = createPictureElement();
+
+// eslint-disable-next-line no-console
+console.log(photos);
